92486ff3c9183c2fce755ba88404183b6b7053de
Rajesh <rajuptl@gmail.com>
2021-09-25 10:31:24 +0530

    First CUT of ETL Application

---
+++ src/main/java/com/smartapps/etl/smartappsetl/SmartappsEtlApplication.java
@@ -0,0 +1,13 @@
+package com.smartapps.etl.smartappsetl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartappsEtlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartappsEtlApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smartapps/etl/smartappsetl/SmartappsEtlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartapps.etl.smartappsetl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartappsEtlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
